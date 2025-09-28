1a11ec99cfc957d6af1ef062d24430f646b76891
shutadayo198 <shutayosh10051013@gmail.com>
2025-09-28 23:07:40 +0900

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,15 +1,1 @@
-const scrollArea = document.getElementById("scrollArea");
-let scrollPosition = 0;
 
-function autoScroll() {
-  scrollPosition += 1;
-  scrollArea.scrollTop = scrollPosition;
-
-  if (scrollPosition >= scrollArea.scrollHeight - scrollArea.clientHeight) {
-    scrollPosition = 0;
-  }
-
-  requestAnimationFrame(autoScroll);
-}
-
-autoScroll();
